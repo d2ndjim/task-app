ef4ad0c27dc60c52d06c6c3132450cb4afcb8cc9
Lekan <jimoridwanlekan@gmail.com>
2023-03-27 12:20:14 +0100

    Added authform component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -27,34 +27,30 @@ const initial = { email: "", password: "", firstName: "", lastName: "" };
 
 export default function AuthForm({ mode }: { mode: "register" | "signin" }) {
   const [formState, setFormState] = useState({ ...initial });
-  const [error, setError] = useState("");
+  // const [error, setError] = useState("");
 
   const router = useRouter();
-  const handleSubmit = useCallback(
-    async (e) => {
-      e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-      try {
-        if (mode === "register") {
-          await register(formState);
-        } else {
-          await signin(formState);
-        }
+    if (mode === "register") {
+      await register(formState);
+    } else {
+      await signin(formState);
+    }
 
-        router.replace("/home");
-      } catch (e) {
-        setError(`Could not ${mode}`);
-      } finally {
-        setFormState({ ...initial });
-      }
-    },
-    [
-      formState.email,
-      formState.password,
-      formState.firstName,
-      formState.lastName,
-    ]
-  );
+    router.replace("/home");
+  
+    setFormState({ ...initial });
+      
+  };
+    // [
+    //   formState.email,
+    //   formState.password,
+    //   formState.firstName,
+    //   formState.lastName,
+    // ]
+;
 
   const content = mode === "register" ? registerContent : signinContent;
 

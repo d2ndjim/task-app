574591d42631feb3a01bed6481f435dc4889d8fd
Lekan <jimoridwanlekan@gmail.com>
2023-03-27 12:51:05 +0100

    Add password management

---
+++ lib/auth.ts
@@ -0,0 +1,6 @@
+import bcrypt from 'bcrypt';
+
+export const hashPassword = (password: string) => bcrypt.hash(password, 10);
+
+export const comparePasswords = (plainTextPassword: string, hashedPassword: string) =>
+  bcrypt.compare(plainTextPassword, hashedPassword);

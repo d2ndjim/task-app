0bcc4b6f84f424510d469afea9bd6e864de3e953
Lekan <jimoridwanlekan@gmail.com>
2023-03-30 21:16:46 +0100

    Modify login

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,9 +2,9 @@ import bcrypt from 'bcrypt';
 import { SignJWT, jwtVerify } from "jose";
 import { db } from "./db";
 
-export const hashPassword = (password: string) => bcrypt.hash(password, 10);
+export const hashPassword = (password) => bcrypt.hash(password, 10);
 
-export const comparePasswords = (plainTextPassword: string, hashedPassword: string) =>
+export const comparePasswords = (plainTextPassword, hashedPassword) =>
   bcrypt.compare(plainTextPassword, hashedPassword);
 
 export const createJWT = (user) => {
@@ -36,7 +36,7 @@ export const getUserFromCookie = async (cookies) => {
 
   const user = await db.user.findUnique({
     where: {
-      id: id as string,
+      id: id,
     },
   });
 
